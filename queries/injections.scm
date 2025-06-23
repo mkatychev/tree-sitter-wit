578747298c620b2caa9c5b04e4915afa47ffd5f0
Mikhail Katychev <mkatych@gmail.com>
2025-06-23 12:33:30 -0500

    comment fixup

--- queries/injections.scm
+++ queries/injections.scm
@@ -1,10 +1,3 @@
-(line_comment
- (doc_comment) @injection.content
- (#set! injection.language "markdown")
+([(line_comment) (doc_comment) (block_comment)] @injection.content
+ (#set! injection.language "comment")
  (#set! injection.include-children))
-
-; TODO resolve nested comment detection with markdown injection
-; (block_comment
-;  (doc_comment) @injection.content
-;  (#set! injection.language "markdown")
-;  (#set! injection.include-children))
